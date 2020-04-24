72b50654b041f06e45285bba7c72cbe7e14952c4
peano88 <ilpeano@gmail.com>
2020-04-24 16:03:11 +0200

    add grpc c++ stub

--- generate_pbs.sh
+++ generate_pbs.sh
@@ -9,5 +9,7 @@ protoc -I $PROTOS_PATH $ITEM_PROTO_FILE --go_out=plugins=grpc:$GOPATH/src
 protoc -I $PROTOS_PATH $COLL_PROTO_FILE --go_out=plugins=grpc:$GOPATH/src
 
 # Cpp
-protoc --proto_path=$PROTOS_PATH --cpp_out=cpp/build $ITEM_PROTO_FILE
-protoc --proto_path=$PROTOS_PATH --cpp_out=cpp/build $COLL_PROTO_FILE
+protoc -I $PROTOS_PATH --grpc_out=. --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` $ITEM_PROTO_FILE
+protoc -I $PROTOS_PATH --cpp_out=cpp/build $ITEM_PROTO_FILE
+protoc -I $PROTOS_PATH --grpc_out=. --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` $COLL_PROTO_FILE
+protoc -I $PROTOS_PATH --cpp_out=cpp/build $COLL_PROTO_FILE
